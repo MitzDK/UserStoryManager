243ced5bc89a50e9a91f10d7cff508db4583a137
MitzDK <victorwilliamjejlskov@gmail.com>
2022-02-07 12:07:15 +0100

    123

--- UserStoryManager/Pages/Index.cshtml.cs
+++ UserStoryManager/Pages/Index.cshtml.cs
@@ -5,6 +5,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using UserStoryManager.Pages.LogIn;
 
 namespace UserStoryManager.Pages
 {
@@ -19,7 +22,10 @@ namespace UserStoryManager.Pages
 
         public void OnGet()
         {
-
+            if (LoginPageModel.LoggedInUser == null)
+            {
+                HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            }
         }
     }
 }
---
+++ UserStoryManager/Pages/LogIn/LogOutPage.cshtml.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace UserStoryManager.Pages.LogIn
+{
+    public class LogOutPageModel : PageModel
+    {
+        public async Task<IActionResult> OnGet()
+        {
+            LoginPageModel.LoggedInUser = null;
+
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            return RedirectToPage("/index");
+        }
+    }
+}
